aff7fe49a25f4df7179e075fb0f9ec00cd74df6f
gurkiratshergill <gurkriatshergill@gmail.com>
2025-02-10 22:56:03 +0530

    Added quantity for items in shopping cart.

--- cart/cart.py
+++ cart/cart.py
@@ -1,3 +1,4 @@
+from store.models import Product
 class Cart():
     def __init__(self, request):
         self.session = request.session
@@ -8,14 +9,25 @@ class Cart():
 
         self.cart = cart
     
-    def add(self, product):
+    def add(self, product, quantity):
         product_id = str(product.id)
+        product_qty = str(quantity)
 
         if product_id not in self.cart:
-            self.cart[product_id] = {'price': str(product.price)}
+            #self.cart[product_id] = {'price': str(product.price)}
+            self.cart[product_id] = int(product_qty)
         
         self.session['cart'] = self.cart  
         self.session.modified = True  
 
     def __len__(self):
         return len(self.cart)
+    
+    def get_products(self):
+        product_ids = self.cart.keys()
+        products = Product.objects.filter(id__in=product_ids)
+
+        return products
+    def get_quantities(self):
+        quantities = self.cart
+        return quantities
